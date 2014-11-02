b5f0e84c559382ac558a554273fb98fa6e81e883
varick <varick@fullsail.edu>
2014-11-01 23:15:27 -0400

    console.log random numbers
    console.log random numbers

--- Wk1/goal1_assign_duel1/js/main.js
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -26,6 +26,21 @@ ANALYZEDuel#1
     function fight(){
         console.log("in the fight function");
 
+        alert(playerOneName+":"+playerOneHealth+" *Start* " +playerTwoName+":"+playerTwoHealth);
+
+        for (var i=0; i<10; i++){
+
+            //random formula is - Math.floor(Math.random() * (max - min) + min);
+
+            var minDamage1 = player1Damage * .5;
+            var minDamage2 = player2Damage * .5;
+            var f1 = Math.floor(Math.random()*(player1Damage-minDamage1) + minDamage1);
+            var f2 = Math.floor(Math.random()*(player2Damage-minDamage2) + minDamage2);
+
+            //console.log(f1);
+            //console.log(f2);
+
+        }
     };
 
     function winnerCheck(){
