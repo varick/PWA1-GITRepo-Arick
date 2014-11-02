3777fba46e7f11677dd76c20b794ef15d6c033fc
varick <varick@fullsail.edu>
2014-11-01 23:47:30 -0400

    End week 1
    End week 1

--- Wk1/goal1_assign_duel1/js/main.js
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -21,7 +21,7 @@ ANALYZEDuel#1
     var playerOneHealth = 100;
     var playerTwoHealth = 100;
 
-    var round = 1;
+    var round = 0;
 
     function fight(){
         console.log("in the fight function");
@@ -37,14 +37,40 @@ ANALYZEDuel#1
             var f1 = Math.floor(Math.random()*(player1Damage-minDamage1) + minDamage1);
             var f2 = Math.floor(Math.random()*(player2Damage-minDamage2) + minDamage2);
 
-            //console.log(f1);
-            //console.log(f2);
+            //inflict damage
+            playerOneHealth-=f1;
+            playerTwoHealth-=f2;
+
+            console.log(playerOneName+":"+playerOneHealth+" *Start* " +playerTwoName+":"+playerTwoHealth);
+
+            var results = winnerCheck();
+            console.log(results);
+
+            if(results==="no winner"){
+                round++;
+                alert(playerOneName+":"+playerOneHealth+" *ROUND "+round+" OVER* " +playerTwoName+":"+playerTwoHealth);
+            }else{
+                alert(results);
+                break;
+            };
 
         }
     };
 
     function winnerCheck(){
-        //code will go here
+        console.log("in winnerCheck FN")
+        var result="no winner";
+
+        if(playerOneHealth<1&&playerTwoHealth<1){
+            result = "You Both Die";
+        }else if(playerOneHealth<1){
+            result =playerTwoName+" WINS!!!"
+        }else if(playerTwoName<1){
+            result =playerOneName+" WINS!!!"
+        };
+
+        return result;
+
     };
 
     /****** The program gets started below ******/
