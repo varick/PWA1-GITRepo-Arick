e65d7e2298f2f81c9527ace8d74db58d5ec23482
varick <varick@fullsail.edu>
2014-11-01 22:37:21 -0400

    Setup Variables

--- Wk1/goal1_assign_duel1/js/main.js
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -6,6 +6,19 @@ ANALYZEDuel#1
 
 //self-executing function
 (function(){
-console.log("FIght!");
 
+    console.log("FIGHT!!!");
+
+    //player name
+    var playerOneName = "Spiderman";
+    var playerTwoName = "Batman";
+
+    //player damage
+    var player1Damage = 20;
+    var player2Damage = 20;
+
+    //player health
+    var playerOneHealth = 100;
+    var playerTwoHealth = 100;
+    
 })();
