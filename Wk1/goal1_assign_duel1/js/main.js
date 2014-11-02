4b2ec2aa945973796b24fbc93ac5e39204967694
varick <varick@fullsail.edu>
2014-11-02 00:32:47 -0400

    Finished comments in Week 1 script
    Finished comments in Week 1 script

--- Wk1/goal1_assign_duel1/js/main.js
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -7,74 +7,79 @@ ANALYZEDuel#1
 //self-executing function
 (function(){
 
-    console.log("FIGHT!!!");
+    console.log("FIGHT!!!");// console.log FIGHT!!!
 
     //player name
-    var playerOneName = "Spiderman";
-    var playerTwoName = "Batman";
+    var playerOneName = "Spiderman";// setting variable player one name to Spiderman
+    var playerTwoName = "Batman";// setting variable player two name to Batman
 
     //player damage
-    var player1Damage = 20;
-    var player2Damage = 20;
+    var player1Damage = 20;// variable player one damage to 20
+    var player2Damage = 20;// variable player two damage to 20
 
     //player health
-    var playerOneHealth = 100;
-    var playerTwoHealth = 100;
+    var playerOneHealth = 100;// variable player one max health to 100
+    var playerTwoHealth = 100;// variable player two max health to 100
 
-    var round = 0;
+    var round = 0;//variable set first round to start at 1
 
-    function fight(){
-        console.log("in the fight function");
+    function fight(){// function for fight
+        console.log("in the fight function");//console.log in the fight function
 
-        alert(playerOneName+":"+playerOneHealth+" *Start* " +playerTwoName+":"+playerTwoHealth);
+        alert(playerOneName+":"+playerOneHealth+" *Start* " +playerTwoName+":"+playerTwoHealth);/*alert Spiderman, and
+        health Start Batman and health*/
 
-        for (var i=0; i<10; i++){
+        for (var i=0; i<10; i++){//variable i for round numbers
 
             //random formula is - Math.floor(Math.random() * (max - min) + min);
 
-            var minDamage1 = player1Damage * .5;
-            var minDamage2 = player2Damage * .5;
-            var f1 = Math.floor(Math.random()*(player1Damage-minDamage1) + minDamage1);
-            var f2 = Math.floor(Math.random()*(player2Damage-minDamage2) + minDamage2);
+            var minDamage1 = player1Damage * .5;// min damage1 = Spiderman damage multiply .5
+            var minDamage2 = player2Damage * .5;// min damage2 = Batman damage multiply .5
+            var f1 = Math.floor(Math.random()*(player1Damage-minDamage1) + minDamage1);/* Math formula to create random
+            and round the number Math.floor*/
+            var f2 = Math.floor(Math.random()*(player2Damage-minDamage2) + minDamage2);/* Math formula to create random
+            and round the number Math.floor*/
 
             //inflict damage
-            playerOneHealth-=f1;
-            playerTwoHealth-=f2;
+            playerOneHealth-=f1;//inflict damage
+            playerTwoHealth-=f2;//inflict damage
 
-            console.log(playerOneName+":"+playerOneHealth+" *Start* " +playerTwoName+":"+playerTwoHealth);
+            console.log(playerOneName+":"+playerOneHealth+" *Start* " +playerTwoName+":"+playerTwoHealth);/* console.log
+            Spiderman player health Start Batman player health*/
 
-            var results = winnerCheck();
-            console.log(results);
+            var results = winnerCheck();//variable result winner check function
+            console.log(results);//console.log winner check
 
-            if(results==="no winner"){
+            if(results==="no winner"){// if no winner add a round loop
                 round++;
-                alert(playerOneName+":"+playerOneHealth+" *ROUND "+round+" OVER* " +playerTwoName+":"+playerTwoHealth);
-            }else{
-                alert(results);
-                break;
+                alert(playerOneName+":"+playerOneHealth+" *ROUND "+round+" OVER* " +playerTwoName+":"+playerTwoHealth);/*
+                alert Spiderman and health round number over Batman and health*/
+            }else{// or there is a winner break function
+                alert(results);// alert outcome of fight
+                break;//break function
             };
 
         }
     };
 
-    function winnerCheck(){
-        console.log("in winnerCheck FN")
-        var result="no winner";
+    function winnerCheck(){//setup winner check function
+        console.log("in winnerCheck FN")//console.log winner check function
+        var result="no winner";//variable result no winner
 
-        if(playerOneHealth<1&&playerTwoHealth<1){
-            result = "You Both Die";
-        }else if(playerOneHealth<1){
-            result =playerTwoName+" WINS!!!"
-        }else if(playerTwoName<1){
-            result =playerOneName+" WINS!!!"
+        if(playerOneHealth<1&&playerTwoHealth<1){//if boh players health goes below 1
+            result = "You Both Die";//they both die
+        }else if(playerOneHealth<1){//Spidermans health goes below 1
+            result =playerTwoName+" WINS!!!"//Batman wins
+        }else if(playerTwoName<1){//Batmans health goes below 1
+            result =playerOneName+" WINS!!!"//Spiderman wins
         };
 
-        return result;
+        return result;//Show result
 
     };
 
     /****** The program gets started below ******/
-    console.log ("program starts here");
-    fight();
+    console.log ("program starts here");// console.log program starts here
+    fight();//begin fight
 
 })();
