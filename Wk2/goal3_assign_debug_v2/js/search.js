cbf8b5ad526724865065ef17cf649be74a51492a
varick <varick@fullsail.edu>
2014-11-05 00:06:56 -0500

    All syntax errors fixed popup window appears
    All syntax errors fixed popup window appears

--- Wk2/goal3_assign_debug_v2/js/search.js
+++ Wk2/goal3_assign_debug_v2/js/search.js
@@ -1,69 +1,71 @@
+
+//Vincent Arick - PWA1 - October 2014
+
+
 // Create privatized scope using a self-executing function
-(function() {//main function
+(function(){//main function
 
 	// Variable initialization (DO NOT FIX ANY OF THE BELOW VAR's)
-	var resultsDIV = document.getElementById("results");//set variable "results" added semicolon at end
-	searchInput = document.forms[0].search;//set variable searchInput, returning form search: added semicolon
-	currentSearch = "";// variable currentSearch not defined: added semicolon, changed to double quotations
+	var resultsDIV = document.getElementById("results"),//set variable "results"
+		searchInput = document.forms[0].search,//set variable searchInput, returning form search: added semicolon
+		currentSearch = ""// variable currentSearch not defined: added semicolon, changed to double quotations
+	;
 
 	// Validates search query
-	var validqte = function (query) {// defining validqte as a function:
+	var validate = function (query){// defining validqte as a function: corrected validate spelling, replaced double equal to one equal
 
 		// Trim whitespace from start and end of search query
-		while (query.charAt(0) = " ") {//loop query 0 = to undefined
+		while(query.charAt(0) === " "){//loop query 0 = to undefined, added 2 equal signs
 			query = query.substring(1, query.length);//query = query substring
-		}
-		;
-		while (query.charAt(query.length - 1) === "") {//loop query -1 undefined equal to input
+		};
+		while (query.charAt(query.length - 1) === ""){//loop query -1 undefined equal to input
 			query = query.substring(0, query.length - 1);//query equals
-			;
+		};//fixed closure error - added curly brace
 
-			// Check search length, must have 3 characters
-			if (query.length < 3) {//if search is less then 3 characters
-				alert("Your search query is too small, try again."); // alert search is to short, try again: added end quotation
+		// Check search length, must have 3 characters
+		if (query.length < 3) {//if search is less then 3 characters
+			alert("Your search query is too small, try again."); // alert search is to short, try again: added end quotation
 
 				// (DO NOT FIX THE LINE DIRECTLY BELOW)
 				searchInput.focus();//search input focus
 				return;//loop back to query input
-			}
-			;
-
+			};// fixed curly braces
+			
 			search(query);//search again for query
-		}
-		;
+		};
+		
+	// Finds search matches
+	var search = function (query) { // declaring variable search equal function query fixed error adding curly brace
 
-		// Finds search matches
-		var search = function (query) {
-		};// declaring variable search equal function query
+			// split the user's search query string into an array
+			var queryArray = query.join("");//variable array equal query join
 
-		// split the user's search query string into an array
-		var queryArray = query.join("");//variable array equal query join
+			// array to store matched results from database.js
+			var results = [];//variable result equals matched result
+//////////
+			// loop through each index of db array
+			for (var i = 0, j = db.length; i < j; i++) {//loop variable equal 0, j equals database length: 0<database length add 1
 
-		// array to store matched results from database.js
-		var results = [];//variable result equals matched result
+				// each db[i] is a single video item, each title ends with a pipe "|"
+				// save a lowercase variable of the video title
+				var dbTitleEnd = db[i].indexOf('|');//variable database 0 seperated by pipe
+				var dbitem = db[i].toLowerCase().substring(0, dbTitleEnd);
 
-		// loop through each index of db array
-		for (var i = 0, j = db.length; i < j; i++) {//loop variable equal 0, j equals database length: 0<database length add 1
+				// loop through the user's search query words
+				// save a lowercase variable of the search keyword
+				for (var ii = 0, jj = queryArray.length; ii < jj; ii++) {//loop variable ii=0, jj=queryArray; if ii<jj add 1
+					var qitem = queryArray[ii].toLowerCase();//variable qitem equal
 
-			// each db[i] is a single video item, each title ends with a pipe "|"
-			// save a lowercase variable of the video title
-			var dbTitleEnd = db[i].indexOf('|');//variable database 0 seperated by pipe
-			var dbitem = db[i].toLowerCase().substring(0, dbTitleEnd);//variable database camal case toLowerCase
+					// is the keyword anywhere in the video title?
+					// If a match is found, push full db[i] into results array
+					var compare = dbitem.indexOf(qitem);//variable compare equal database index item
+					if (compare !== -1) {//if variable compare does not equal -1
+						results.push(db[i]);//result push database
 
-			// loop through the user's search query words
-			// save a lowercase variable of the search keyword
-			for (var ii = 0, jj = queryArray.length; ii < jj; ii++) {//loop variable ii=0, jj=queryArray; if ii<jj add 1
-				var qitem = queryArray[ii].toLowerCase();//variable qitem equal
+					};
 
-				// is the keyword anywhere in the video title?
-				// If a match is found, push full db[i] into results array
-				var compare = dbitem.indexOf(qitem);//variable compare equal database index item
-				if (compare !== -1) {//if variable compare does not equal -1
-					results.push(db[i]);//result push database
-				}
-				;
-				;
-				;
+				};//added semicolon
+			};//added semicolon
 
 				results.sort();//sort result
 
@@ -72,17 +74,14 @@
 					noMatch();//no match
 				} else {//else
 					showMatches(results);//show result
-				}
-				;
-			}
-			;
-
+				};
+			};
+/////////////
 			// Put "No Results" message into page (DO NOT FIX THE HTML VAR NOR THE innerHTML)
 			var noMatch = function () {//variable no match function
 				var html = ("") + //variable html equals undefined plus paragraphs
 						'<p>No Results found.</p>' +//paragraph No Results Found
 						'<p style="font-size:10px;">Try searching for "JavaScript".  Just an idea.</p>'//paragraph try searching
-					;
 				resultsDIV.innerHTML = html;//result innerHTML
 			};
 
@@ -93,10 +92,10 @@
 				var html = '<p>Results</p>',//Result in html paragraph
 					title,
 					url
-					;
+				;
 
 				// loop through all the results search() function
-				for (var i = 0, j = results.length; i < j; i++) {//loop variable results.length
+				for (var i=0, j=results.length; i < j; i++) {//loop variable results.length
 
 					// title of video ends with pipe
 					// pull the title's string using index numbers
@@ -108,8 +107,7 @@
 
 					// make the video link - THE NEXT LINE IS CORRECT.
 					html += '<p><a href=' + url + '>' + title + '</a></p>';//make video link
-				}
-				;
+				};
 				resultsDIV.innerHTML = html; //THIS LINE IS CORRECT.
 			};
 
@@ -117,14 +115,11 @@
 			// THE LINE DIRECTLY BELOW IS CORRECT
 			document.forms[0].onsubmit = function () {//submit function
 				var query = searchInput.value;// variable query equal searchInput
-				validqte(query);//validate query
+				validate(query);//validate query, corrected validate misspelling
 
 				// return false is needed for most events - this will be reviewed in upcoming course material
 				// THE LINE DIRECTLY BELOW IS CORRECT
-				return false
+				return false;//added semicolon
 			};//end loop
-		}
-
-	}
 
 })();
