5c138d834a0e34ef765376d603c8956c29822d6f
varick <varick@fullsail.edu>
2014-11-11 21:08:12 -0500

    100 percent debug fixed
    100 percent debug fixed

--- Wk3/goal3_assign_debug_v3/js/search.js
+++ Wk3/goal3_assign_debug_v3/js/search.js
@@ -10,7 +10,6 @@
 		searchInput = document.forms[0].search,//set variable searchInput, returning form search: added semicolon
 		currentSearch = ""// variable currentSearch not defined: added semicolon, changed to double quotations
 	;
-
 	// Validates search query
 	var validate = function (query){// defining validqte as a function: corrected validate spelling, replaced double equal to one equal
 
@@ -38,7 +37,7 @@
 	var search = function (query) { // declaring variable search equal function query fixed error adding curly brace
 
 			// split the user's search query string into an array
-			var queryArray = query.join("");//variable array equal query join
+			var queryArray = query.split("");//fixed error changed .join to .split 
 
 			// array to store matched results from database.js
 			var results = [];//variable result equals matched result
@@ -49,12 +48,12 @@
 				// each db[i] is a single video item, each title ends with a pipe "|"
 				// save a lowercase variable of the video title
 				var dbTitleEnd = db[i].indexOf('|');//variable database 0 seperated by pipe
-				var dbitem = db[i].toLowerCase().substring(0, dbTitleEnd);
+				var dbitem = db[i].toLowerCase().substring(0, dbTitleEnd);//fixed camal casing
 
 				// loop through the user's search query words
 				// save a lowercase variable of the search keyword
 				for (var ii = 0, jj = queryArray.length; ii < jj; ii++) {//loop variable ii=0, jj=queryArray; if ii<jj add 1
-					var qitem = queryArray[ii].toLowerCase();//variable qitem equal
+					var qitem = queryArray[ii].toLowerCase();//fixed camal casing
 
 					// is the keyword anywhere in the video title?
 					// If a match is found, push full db[i] into results array
@@ -70,13 +69,16 @@
 				results.sort();//sort result
 
 				// Check that matches were found, and run output functions
-				if (results.length = 0) {//if result doesn't equal 0
+				if (results.length === 0) {// added 2 == to fix the for loop
 					noMatch();//no match
 				} else {//else
 					showMatches(results);//show result
 				};
 			};
 /////////////
+
+
+
 			// Put "No Results" message into page (DO NOT FIX THE HTML VAR NOR THE innerHTML)
 			var noMatch = function () {//variable no match function
 				var html = ("") + //variable html equals undefined plus paragraphs
@@ -84,23 +86,23 @@
 						'<p style="font-size:10px;">Try searching for "JavaScript".  Just an idea.</p>'//paragraph try searching
 				resultsDIV.innerHTML = html;//result innerHTML
 			};
-
 			// Put matches into page as paragraphs with anchors
 			var showMatches = function (results) {//variable showMatch function
-
 				// THE NEXT 4 LINES ARE CORRECT.
 				var html = '<p>Results</p>',//Result in html paragraph
 					title,
 					url
 				;
+				//console.log("Test");//console.log for testing
 
 				// loop through all the results search() function
 				for (var i=0, j=results.length; i < j; i++) {//loop variable results.length
 
+
 					// title of video ends with pipe
 					// pull the title's string using index numbers
 					titleEnd = results[i].indexOf('|');//result name variable titleEnd
-					title = results[i].subString(0, titleEnd);//title = result titleEnd
+					title = results[i].substring(0, titleEnd);//corrected camal casing for substring
 
 					// pull the video url after the title
 					url = results[i].substring(results[i].indexOf('|') + 1, results[i].length);//pull video url
